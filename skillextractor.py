e70df8fc5629239e60dd721350822488bed56e83
flopezde <flopezde@users.noreply.github.com>
2017-03-24 18:01:29 -0600

    Update skillextractor.py

--- skillextractor.py
+++ skillextractor.py
@@ -1,6 +1,11 @@
 import json
 import re
 
+skillsDict = {}
+
+def findWholeWord(w):
+    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE)
+
 def extractSkills(file):
     with open(file) as data_file:
         data = json.load(data_file)
@@ -9,8 +14,44 @@ def extractSkills(file):
         for key in section:
             skillString = section[key]
             skillList = [skillList.strip() for skillList in re.split(',|:', skillString)]
+    return skillList
 
-    for x in skillList:
-        print(x)
+from dateutil.parser import parse
+from datetime import datetime
 
-extractSkills(input("Filename: "))
+def extractDates(file, skillList):
+    with open(file) as data_file:
+        data = json.load(data_file)
+
+    for section in data["work_experience"]:
+        delta = 0
+        if "date_start" in section.keys():
+            startDate = parse(section["date_start"])
+        if "date_end" in section.keys():
+            endDate = parse(section["date_end"])
+        else:
+            endDate = datetime.today()
+        delta = endDate - startDate
+        #print(delta.total_seconds()/86400/30)
+        if "text" in section.keys():
+            text = section["text"].lower()
+            print(text)
+            for x in skillList:
+                print(x.lower())
+                if re.search(r"[^a-z]"+re.escape(x.lower())+r"[^a-z]", text) != None:
+                    print("Found")
+                    print(x)
+                    print("in")
+                    print(text)
+                    skillsDict[x] += delta.total_seconds()
+                    
+
+filename = input("Filename: ")
+skillList = extractSkills(filename)
+for x in skillList:
+  skillsDict[x] = 0
+extractDates(filename, skillList)
+for key in skillsDict:
+    print(key)
+    print(skillsDict[key])
+
