58b0e197b416e35d0e8a00bfba686add66d8830d
Mehran Mahmoudi <mehr.moudi@gmail.com>
2017-03-31 12:21:23 -0600

    Edited raw_skill_extractor

--- raw_skill_extractor.py
+++ raw_skill_extractor.py
@@ -9,16 +9,13 @@ def find_whole_word(word):
     return re.compile(r'\b({0})\b'.format(word), flags=re.IGNORECASE)
 
 
-def extract_skills(json_file):
-    with open(json_file) as data_file:
-        data = json.load(data_file)
-
-    if 'skills' in data:
+def extract_skills(resume_json):
+    if 'skills' in resume_json:
         skill_list = list()
-        for section in data["skills"]:
+        for section in resume_json["skills"]:
             for key in section:
                 skill_string = section[key]
-                skill_list += [x.strip() for x in re.split(',|:', skill_string)]
+                skill_list += [y for y in [x.replace("\n", "").strip() for x in re.split("\s*[^A-Za-z0-9\s+\-\(\)]\s*", skill_string)] if y != '']
         return skill_list
 
 
@@ -36,7 +33,7 @@ def save_skill_to_database(skill_name):
 
 def parse_resume(resume, output):
     chdir('ResumeParser/ResumeTransducer')
-    system('export GATE_HOME="..\GATEFiles"')
+    system('export GATE_HOME="../GATEFiles"')
     command = ("java -cp 'bin/*:../GATEFiles/lib/*:../GATEFiles/bin/gate.jar:lib/*' "
                "code4goal.antony.resumeparser.ResumeParserProgram "
                "../../" + resume + " "
@@ -45,18 +42,25 @@ def parse_resume(resume, output):
     chdir('../..')
 
 
-def run(resumes_dir):
+def parse_directory(resumes_dir):
     pdf_files = get_resume_files(resumes_dir)
     temp_output_name = 'parsed.json'
-    for resume in pdf_files:
-        parse_resume(resume, temp_output_name)
-        skills_list = extract_skills(temp_output_name)
+    for resume_file in pdf_files:
+        parse_resume(resume_file, temp_output_name)
+        with open(temp_output_name, 'r') as json_file:
+            RawResume.create(resume_json=json_file.read())
+        remove(temp_output_name)
+
+
+if __name__ == '__main__':
+    if len(RawResume.select()) < 1:
+        parse_directory('resumes')
+
+    for resume in RawResume.select():
+        resume_json = json.loads(resume.resume_json)
+        skills_list = extract_skills(resume_json)
         if skills_list is not None:
             for skill_name in skills_list:
                 save_skill_to_database(skill_name.lower())
         else:
             print("No skills!")
-        remove(temp_output_name)
-
-if __name__ == '__main__':
-    run('resumes')
